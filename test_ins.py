36dbe960bd14b28c0d8e08071853838aa47c4250
smohi <smohi.wd@gmail.com>
2024-09-25 23:29:56 +0900

    All necessary packages were installed

---
+++ test_ins.py
@@ -0,0 +1,9 @@
+import speech_recognition as sr
+import pyttsx3
+from googletrans import Translator
+import nltk
+from gtts import gTTS
+from vosk import Model, KaldiRecognizer
+
+print("All necessary packages were imported successfully!")
+
