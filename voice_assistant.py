bd4275ea1b4f8c6984b41bbb601eec76b09b6961
smohi <smohi.wd@gmail.com>
2024-10-02 18:54:49 +0900

    changed voice to F En-Au

--- voice_assistant.py
+++ voice_assistant.py
@@ -5,7 +5,7 @@ recognizer = sr.Recognizer()
 engine = pyttsx3.init()
 
 voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)
+engine.setProperty('voice', voices[17].id) #33 = female-US 17= female AU
 
 
 unknown_v = 0
